c7400768601c3d26af5b51109b6a9344dd9d17f4
RonaldodeLema <vanthao000009@gmail.com>
2024-04-07 20:38:07 +0700

    update:call api to ui end user

--- microservices/end_user_ui/src/utils/api.js
+++ microservices/end_user_ui/src/utils/api.js
@@ -1,9 +1,11 @@
 const BASE_URL = "http://127.0.0.1:3000/api";
 const HOST_NAME = "http://127.0.0.1:4000"
 const BASE_RENTAL_URL = "http://127.0.0.1:3002/api";
+const BASE_OFFER_URL = "http://127.0.0.1:3001/api";
 
 export {
   BASE_URL,
   HOST_NAME,
-  BASE_RENTAL_URL
+  BASE_RENTAL_URL,
+  BASE_OFFER_URL
 }
